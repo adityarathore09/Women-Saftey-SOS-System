88962af7a2447b440db881b8d18087b20d43fa9b
adityarathore09 <72064472+adityarathore09@users.noreply.github.com>
2026-03-29 18:46:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/Women-Safety-SOS-System/', // ✅ VERY IMPORTANT
+  base: '/Women-Saftey-SOS-System/', // ✅ VERY IMPORTANT
   plugins: [react()],
 })
