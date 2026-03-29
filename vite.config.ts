f1e2cc523b564b6231fdbf8e8a8f985f4c2ad93c
adityarathore09 <72064472+adityarathore09@users.noreply.github.com>
2026-03-29 16:58:59 +0530

    Fix import for react plugin in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Women-Safety-SOS-System/',   // ✅ MUST ADD THIS
+  base: '/Women-Safety-SOS-System/',   // ✅ THIS IS THE FIX
   plugins: [react()],
 })
